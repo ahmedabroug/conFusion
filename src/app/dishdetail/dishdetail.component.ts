907f30c4a7804ba218fba7638e633675fae2240a
Ahmed Abroug <ahm3dabroug@gmail.com>
2018-10-03 10:51:55 +0100

    Animations part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
+import { visibility } from '../animations/app.animation';
 
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
@@ -19,17 +19,7 @@ import { switchMap } from 'rxjs/operators';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility()
   ]
 })
 export class DishdetailComponent implements OnInit {
